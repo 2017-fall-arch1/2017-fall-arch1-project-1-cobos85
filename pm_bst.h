a309c48c08fc6132a20fc5da4e91e981c9022640
Brandon Cobos <cobosraider545@gmail.com>
2017-10-16 00:08:44 -0600

    Initial commit and header file

---
+++ pm_bst.h
@@ -0,0 +1,42 @@
+#ifndef bst_included
+#define bst_included
+
+
+/* a binary search tree node */
+typdef struct bstNode
+{
+  
+  char *name;
+  struct bstNode *left;
+  struct bstNode *right;
+  
+}bstNode;
+
+/* to find minimum value in tree */
+bstNode* FindMin(bstNode *node);
+
+/* to find maximum value in tree */
+bstNode* FindMax(bstNode *node);
+
+/* to insert new name into bst */
+bstNode* InsertName(bstNode *node, char *name);
+
+/* to delete given name from bst */
+bstNode* DeleteName(bstNode *node, char *name);
+
+/* to find given name in bst */
+bstNode* FindName(bstNode *node, char *name);
+
+/* to print tree in order */
+void PrintInOrder(bstNode *node);
+
+/* to print tree in preorder */
+void PrintPreorder(bstNode *node);
+
+/* to print tree in postorder */
+void PrintPostorder(bstNode *node);
+
+
+
+
+#endif
